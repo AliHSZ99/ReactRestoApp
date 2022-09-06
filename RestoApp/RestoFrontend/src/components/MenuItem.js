8b59224882b2b0f1f0a76ae7d22c9d7db0eb4c9c
Ali <alihsz2002@gmail.com>
2022-09-06 15:55:38 -0400

    Testing if Strapi items stay because they were deleted...

--- RestoApp/RestoFrontend/src/components/MenuItem.js
+++ RestoApp/RestoFrontend/src/components/MenuItem.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function MenuItem() {
     return (
-        <div>MenuItem</div>
+        <div className='border'>
+            {/* Use gatsby image here: WATCH YOUTUBE VIDEO */}
+            <img></img>
+            <h2>Menu Item Name</h2>
+            <p>Menu Item Description</p>
+            <p>Price</p>
+        </div>
     );
 }
